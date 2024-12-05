f311543a06a7fc123d9b4623148df31ca87cdc0d
AdityaDinata <adityadinata647@gmail.com>
2024-12-05 16:39:16 +0800

    test

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { RouterProvider, createBrowserRouter, Navigate } from "react-router-dom";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import ForgotPass from "./pages/Auth/forgotPass";
+import VerifyCode from "./pages/Auth/verifyCode";
+import ResetPassword from "./pages/Auth/resetPassword";
+const routes = [
+  { path: "/", element: <Navigate to="/login" replace /> },
+  { path: "/register", element: <Register /> }, 
+  { path: "/aturulang", element: <ForgotPass /> },
+  { path: "*", element: <div>404 - Page Not Found</div> },
+  { path: "/login", element: <Login /> },
+  { path: "/reset", element: <ResetPassword/> },
+  { path: "/verify", element: <VerifyCode /> },
+];
+
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_startTransition: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
